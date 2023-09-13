0f67b98801159f944b99b2f14f9a75fb0e2c8930
fgilvan <gilvan_226@live.com>
2023-09-12 22:02:58 -0300

    Adicionando validações com FluentValidation

---
+++ Server/DigitalMenu.Application/Model/Product/ProductValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalMenu.Application.Model.Product
+{
+    public class ProductValidator: AbstractValidator<ProductModel>
+    {
+        public ProductValidator() 
+        {
+            ValidatorName();
+            ValidatorDescription();
+        }
+
+        #region PRIVATE METHODS
+
+        private void ValidatorDescription()
+        {
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("Descrição do produto é obrigatório.");
+        }
+
+        private void ValidatorName()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Nome do produto é obrigatório.");
+
+            RuleFor(x => x.Name)
+                .MaximumLength(50)
+                .WithMessage("Nome do produto não pode ultrapassar 50 caracteres");
+        }
+
+        #endregion
+    }
+}
--- Server/DigitalMenu.Infrastructure/DependencyInjection.cs
+++ Server/DigitalMenu.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,12 @@
 ﻿using DigitalMenu.Application.Interfaces;
+using DigitalMenu.Application.Model.Product;
 using DigitalMenu.Application.Services;
 using DigitalMenu.Core.Interfaces;
 using DigitalMenu.Infrastructure.Persistence;
 using DigitalMenu.Infrastructure.Persistence.Repository;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,6 +24,7 @@ namespace DigitalMenu.Infrastructure
         public static IServiceCollection AddInfrastructServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductValidator>());
 
             //Services
             services.AddScoped<IServiceProduct, ServiceProduct>();
