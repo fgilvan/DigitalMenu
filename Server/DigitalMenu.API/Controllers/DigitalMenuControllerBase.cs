1f146fcf4917ae7e5e0f2e301ca8a425dcb01333
fgilvan <gilvan_226@live.com>
2023-10-24 21:52:23 -0300

    Adicionando documentação Swagger.

--- Server/DigitalMenu.API/Controllers/DigitalMenuControllerBase.cs
+++ Server/DigitalMenu.API/Controllers/DigitalMenuControllerBase.cs
@@ -3,21 +3,33 @@ using DigitalMenu.Application.Interfaces;
 using DigitalMenu.Application.Model;
 using DigitalMenu.Application.Model.Product;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace DigitalMenu.API.Controllers
 {
-    public abstract class DigitalMenuControllerBase<TModel> : ControllerBase 
+    public abstract class DigitalMenuControllerBase<TModel> : ControllerBase
         where TModel : ModelBase
     {
         protected abstract string Route { get; }
 
+        protected static Type TypeModel => typeof(TModel);
+
+
         protected readonly IServiceBase<TModel> _service;
+
 
         public DigitalMenuControllerBase(IServiceBase<TModel> service)
         {
             _service = service;
         }
 
+        /// <summary>
+        /// Obtém a lista de registros.
+        /// </summary>
+        /// <returns>A lista de registros.</returns>
+        /// <response code="200">Registros encontrado.</response>
+        /// <response code="500">Erro na aplicação.</response>
         [HttpGet]
         public async Task<IActionResult> Get()
         {
@@ -33,8 +45,16 @@ namespace DigitalMenu.API.Controllers
             }
         }
 
+        /// <summary>
+        /// Obtém registro por id.
+        /// </summary>
+        /// <param name="id">O identificador do registro.</param>
+        /// <returns>O registro no banco de dados.</returns>
+        /// <response code="200">Registro encontrado.</response>
+        /// <response code="404">Registro não encontrado.</response>
+        /// <response code="500">Erro na aplicação.</response>
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(Guid id)
+        public virtual async Task<IActionResult> Get(Guid id)
         {
             try
             {
@@ -52,6 +72,14 @@ namespace DigitalMenu.API.Controllers
             }
         }
 
+        /// <summary>
+        /// Realiza o cadastro do registro no banco de dados.
+        /// </summary>
+        /// <param name="model">O registro a ser cadastrado.</param>
+        /// <returns>O registro cadastrado no banco de dados.</returns>
+        /// <response code="201">Registro cadastrado.</response>
+        /// <response code="400">Registro inválido.</response>
+        /// <response code="500">Erro na aplicação.</response>
         [HttpPost]
         public async Task<IActionResult> Post(TModel model)
         {
@@ -61,24 +89,35 @@ namespace DigitalMenu.API.Controllers
 
                 return Created(Route + model.Id, model);
             }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
             catch (Exception ex)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
 
+        /// <summary>
+        /// Realiza a atualização do registro no banco de dados.
+        /// </summary>
+        /// <param name="id">O identificador do registro a ser atualizado.</param>
+        /// <param name="model">O registro a ser atualizado.</param>
+        /// <returns>O registro atualizado no banco de dados.</returns>
+        /// <response code="201">Registro cadastrado.</response>
+        /// <response code="400">Registro inválido.</response>
+        /// <response code="404">Registro não encontrado.</response>
+        /// <response code="500">Erro na aplicação.</response>
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(Guid id, TModel model)
         {
             try
             {
+                if (id != model.Id)
+                {
+                    return BadRequest();
+                }
+
                 await _service.Update(id, model);
 
-                return Created(Route + model.Id, model);
+                return Ok(model);
             }
             catch (NotFoundException ex)
             {
@@ -90,6 +129,15 @@ namespace DigitalMenu.API.Controllers
             }
         }
 
+
+        /// <summary>
+        /// Realiza a exclusão do registro no banco de dados.
+        /// </summary>
+        /// <param name="id">O id do registro a ser excluído.</param>
+        /// <returns>O status da exclusão no banco de dados.</returns>
+        /// <response code="200">Registro excluído.</response>
+        /// <response code="404">Registro não encontrado.</response>
+        /// <response code="500">Erro na aplicação.</response>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(Guid id)
         {
--- Server/DigitalMenu.API/Program.cs
+++ Server/DigitalMenu.API/Program.cs
@@ -1,6 +1,8 @@
 using DigitalMenu.Application.Common.Automapper;
 using DigitalMenu.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    c.IncludeXmlComments(xmlPath);
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "DigitalMenu",
+            Description = "Simples API em .NET 7",
+            Version = "v1",
+            Contact = new OpenApiContact()
+            {
+                Name = "Gilvan Jacó Filho",
+                Url = new Uri("https://github.com/fgilvan"),
+            }
+        });
+});
+
 builder.Services.AddHealthChecks();
 builder.Services.AddMvc();
 builder.Services.AddInfrastructServices(builder.Configuration);
@@ -19,6 +41,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+   
     app.UseSwagger();
     app.UseSwaggerUI();
 }
